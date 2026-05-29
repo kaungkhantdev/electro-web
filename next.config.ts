2fafd80266663b2252bfb323a327efa883b1e864
kaungkhantcodigo <kaungkhantzaw@codigo.co>
2026-05-29 07:39:58 +0700

    feat: prepare for product detail

--- next.config.ts
+++ next.config.ts
@@ -9,6 +9,11 @@ const nextConfig: NextConfig = {
         port: "8000",
         pathname: "/**",
       },
+      {
+        protocol: "https",
+        hostname: "www.apple.com",
+        pathname: "/**",
+      },
     ],
   },
 };
